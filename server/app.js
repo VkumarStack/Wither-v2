1176e1acbec0af05afedcf5274b7a330e390c022
VkumarStack <vksogi@gmail.com>
2022-10-28 12:21:57 -0700

    Create file structure and install dependencies

---
+++ server/app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+var express = require('express');
+var cors = require('cors');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const mongoose = require("mongoose");
+const mongoDB = process.env.DATABASE_URL;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+module.exports = app;
