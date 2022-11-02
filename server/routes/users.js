8b2882c6249388684ac84bfb31d78d990e8f8349
Nick Johnson <nick@Nicks-MacBook-Pro-2.local>
2022-11-02 16:55:56 -0700

    Added controllers, updated models, created json objects for unique user IDS.

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-<<<<<<< HEAD
 /* Controller vars */
 const users_controller = require("../controllers/usersController");
 
@@ -13,20 +12,6 @@ router.get('/', function(req, res, next) {
 
 router.get("/dummy", function (req, res) {
   res.send("Dummy user page");
-=======
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('User Home Page');
-});
-router.get('/:id', function(req, res, next) {
-  user = {username: req.params.id, bio: "Bio"}
-  res.json(user);
-});
-/* POST users listing. */
-router.post('/:id', function(req, res, next) {
-  res.send('respond with a resource');
->>>>>>> d88f614cf0cab32b0bbdbf3f484e5a0007f32111
 });
 
 router.get("/:userID", users_controller.getUserID);
