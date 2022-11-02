d88f614cf0cab32b0bbdbf3f484e5a0007f32111
wonjinlee1202 <53541049+wonjinlee1202@users.noreply.github.com>
2022-11-02 16:44:02 -0700

    Update users.js
    Dummy data for user route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  res.send('User Home Page');
+});
+router.get('/:id', function(req, res, next) {
+  user = {username: req.params.id, bio: "Bio"}
+  res.json(user);
+});
+/* POST users listing. */
+router.post('/:id', function(req, res, next) {
   res.send('respond with a resource');
 });
 
