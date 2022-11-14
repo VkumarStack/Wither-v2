5585b12d12d62727ecf2385b0b5f85769d39ddd0
Ash Loong <ashleyloong@ucla.edu>
2022-11-13 17:13:09 -0800

    Updated createPost component

--- client/src/Routes/Profile.js
+++ client/src/Routes/Profile.js
@@ -2,13 +2,14 @@ import React from "react";
 import { ReactDOM } from "react";
 import '../Stylesheets/profile.css';
 import Post from './Post';
+import HeaderBar from './HeaderBar';
+import CreatePost from "./CreatePost";
 
 class UserInfo extends React.Component {
     constructor(props) {
         // Props: User ID (to use for backend calls), User, Username, Bio
         super(props);
         this.state = {
-            // Todo: add profile picture
             id: this.props.userinfo.id,
             user: this.props.userinfo.user, 
             username: this.props.userinfo.username,
@@ -39,13 +40,13 @@ class Profile extends React.Component {
     render() {
         return (
             <div className="Profile">
-                {/* Todo: On entering the search, display a list of Users matching the query below the search input element */}
-                <input type="search" id="user-search" placeholder="Search Wither" />
+                <HeaderBar></HeaderBar>
                 <div className='rowC' /* left column for user info, right column for posts */>
                     <div>
                         <UserInfo userinfo ={{id: 111, user: "Joe Bruin", username: "@joebruin", bio: "official mascot of UCLA | boyfriend of @josiebruin"}}/>
                     </div>
                     <div className="posts">
+                        <CreatePost></CreatePost>
                         <Post post={{id: 111, user: "Joe Bruin", text: "I love UCLA!", likes: ["John Doe", "Doe John"], dislikes: ["Traveler Trojan", "Oski Bear"]}}/>
                         <Post post={{id: 111, user: "Joe Bruin", text: "Crazy how I never miss a football game.", likes: ["John Doe", "Doe John"], dislikes: ["Traveler Trojan", "Oski Bear"]}}/>
                         <Post post={{id: 111, user: "Joe Bruin", text: "I miss Josie", likes: ["John Doe", "Doe John"], dislikes: ["Traveler Trojan", "Oski Bear"]}}/>
