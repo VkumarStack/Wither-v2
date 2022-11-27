2a15ce527ff3d3452886d1c2336d448b587446d7
Ash Loong <ashleyloong@ucla.edu>
2022-11-27 02:22:24 -0800

    Created Follow component

---
+++ client/src/Routes/Follow.js
@@ -0,0 +1,61 @@
+import React from "react";
+import '../Stylesheets/follow.css';
+
+class Follow extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: sessionStorage.getItem("user"),
+            errorMessage: false
+        };
+    }
+
+    async handleSubmit(e) {
+        if (this.state.user === null)
+            return;
+        e.preventDefault();
+        let response = await fetch(`http://localhost:8080/users/${this.state.user}/follow`, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + sessionStorage.getItem("token"),
+            },
+            body: JSON.stringify({username: this.state.user, followers: this.props.current})
+        })
+        response = await response.json();
+        console.log(response);
+        if (response.Error)
+        {
+            // If the user is logged in but their sign in token is bad (sign in token expires after 2 hours), log them out and reload the page
+            if (response.TokenError)
+            {
+                sessionStorage.clear();
+                window.location.reload(true);
+            }
+            else 
+            {
+                // do not post 
+                this.setState({errorMessage: response.Error});
+                setTimeout(() => {this.setState({errorMessage: null})}, 20000);
+            }
+        }
+        else
+        {
+            // This is altering the parent component's (Profile) state, which causes it to re-render the UserInfo 
+            // component with the new follower
+            this.props.updateFollow();
+        }
+    }
+
+    render() {
+        return (
+            <div className="Follow">
+                <div className="follow-button" onClick={(e) => this.handleSubmit(e)}> Follow </div>
+                    {   (this.state.errorMessage !== null) &&
+                        <h1> {this.state.errorMessage} </h1>
+                    }
+            </div>
+        );
+    }
+}
+export default Follow;
